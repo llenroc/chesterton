30d3e3e450644c5847bc2cc80f81dfc1e57d7398
westonplatter <westonplatter@gmail.com>
2018-10-25 09:03:52 -0600

    CORS stuff

--- server.py
+++ server.py
@@ -21,6 +21,15 @@ env = Environment(loader=FileSystemLoader('templates'))
 
 
 class BaseHandler(tornado.web.RequestHandler):
+     def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
     def render_json(self, data):
         self.write(json.dumps(data))
         self.finish()
