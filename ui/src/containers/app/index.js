68ee0130cc6dabf62f8bc310069a3cda7222a10e
westonplatter <westonplatter@gmail.com>
2018-10-27 15:16:24 -0600

    add account page.

--- ui/src/containers/app/index.js
+++ ui/src/containers/app/index.js
@@ -2,17 +2,20 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
+import Account from '../account'
 
 const App = () => (
   <div>
     <header>
       <Link to="/">Home</Link>
+      <Link to="/account">Account</Link>
       <Link to="/about-us">About</Link>
     </header>
 
     <main>
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
+      <Route exact path="/account" component={Account} />
     </main>
   </div>
 )
