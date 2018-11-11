4339fd4f1b26d8c3f7d2fbbb297242e14bddb111
westonplatter <westonplatter@gmail.com>
2018-11-11 16:34:30 -0700

    clean up.

--- ui/src/containers/app/index.js
+++ ui/src/containers/app/index.js
@@ -23,17 +23,8 @@ import {
 class AppHeader extends React.Component {
     constructor(props) {
       super(props);
-
-      this.toggle = this.toggle.bind(this);
-      this.state = {
-        isOpen: false
-      };
     }
-    toggle() {
-      this.setState({
-        isOpen: !this.state.isOpen
-      });
-    }
+    
     render() {
       return (
         <div>
